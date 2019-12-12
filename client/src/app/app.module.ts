4ded45426cc669e8b638272b25e57462bf84e2d3
Jeannie <hello@jeannierombough.com>
2019-12-11 21:11:18 -0500

    Add home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <section class="hero is-info is-fullheight is-bold">
+    <div class="hero-body">
+    <div class="container">
+
+      <h1 class="title">Home page works!</h1>
+
+    </div>
+    </div>
+    </section>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
