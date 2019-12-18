abf027d8c8bc501bfe048ce0b8e0349e3d73f99c
Jeannie <hello@jeannierombough.com>
2019-12-18 18:52:44 -0500

    Clean up comments

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!-- header -->
-    <app-header></app-header>
-    <!-- routes will be rendered here -->
-    <router-outlet></router-outlet>
-    <!-- footer -->
-    <app-footer></app-footer>
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'client';
 }
