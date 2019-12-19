a3795f0acd6b9db2d964a36a83a7cf9b22cc07b3
jp-2019 <jeannie.wesell@gmail.com>
2019-12-19 11:38:31 -0500

    Update ApplicationSecurityConfig.java

--- server/src/main/java/com/ghost/server/security/ApplicationSecurityConfig.java
+++ server/src/main/java/com/ghost/server/security/ApplicationSecurityConfig.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.ghost.server.security.ApplicationUserRole.ADMIN;
+import static com.ghost.server.security.ApplicationUserRole.MEMBER;
+
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -27,8 +30,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(ADMIN.name()) //member cant access the item api
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -41,13 +44,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles("MEMBER") //ROLE_MEMBER (in lieu of student)
+                .roles(MEMBER.name()) //ROLE_MEMBER (in lieu of student)
                 .build();
 
         UserDetails jeannieUser = User.builder()
                 .username("jeannie")
                 .password(passwordEncoder.encode("password123"))
-                .roles("ADMIN") //ROLE_ADMIN
+                .roles(ADMIN.name()) //ROLE_ADMIN
                 .build();
 
         return new InMemoryUserDetailsManager(
