2fd204d9a698183010b6fedd2e944d65882d9a0b
jp-2019 <jeannie.wesell@gmail.com>
2019-12-19 08:56:55 -0500

    User

---
+++ server/src/main/java/com/ghost/server/user/UserController.java
@@ -0,0 +1,28 @@
+package com.ghost.server.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    private static final List<User> USERS = Arrays.asList(
+            new User(1, "Jane Bond"),
+            new User(2, "Anna Smith"),
+            new User(3, "Sara Keene")
+    );
+
+    @GetMapping(path = "{userId}")
+    public User getUser(@PathVariable("userId") Integer userId) {
+        return USERS.stream()
+                .filter(user -> userId.equals(user.getUserId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("User " + userId + " does not exist"));
+    }
+}
