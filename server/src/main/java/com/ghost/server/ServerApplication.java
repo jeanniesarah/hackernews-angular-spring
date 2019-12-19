9bc3d38266e134631f509dee18c7665855641a56
jp-2019 <jeannie.wesell@gmail.com>
2019-12-19 08:37:43 -0500

    Running success

--- server/src/main/java/com/ghost/server/ServerApplication.java
+++ server/src/main/java/com/ghost/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.ghostblog.server;
+package com.ghost.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/src/test/java/com/ghost/server/ServerApplicationTests.java
+++ server/src/test/java/com/ghost/server/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ghostblog.server;
+package com.ghost.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
