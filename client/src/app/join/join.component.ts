756e3ae9ef314f4e2f236a0066f0d997e7224514
jeanniesarah <jeannie.wesell@gmail.com>
2020-01-14 10:39:00 -0500

    add proxy pattern

--- client/src/app/join/join.component.ts
+++ client/src/app/join/join.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
     <section class="hero is-fullheight is-bold">
       <div class="hero-body">
         <div class="container">
-          <h1 class="large-title">join us page works</h1>
+          <h1 class="large-title">join us page in the works</h1>
           <!-- TODO: add education funnel content -->
         </div>
       </div>
